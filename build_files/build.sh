ef2d1136bf9aa36ff174bd06f160348c794fc647
KirrimK <KirrimK@users.noreply.github.com>
2025-10-12 18:10:27 +0200

    Remove comment

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,14 +2,6 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-
 dnf5 remove -y waydroid* rom-properties* sunshine input-remapper webapp-manager lutris clapper
 
 rm -f /usr/share/applications/waydroid*.desktop
